ff44fb66c94f587a89a3f7a2536677c50fb9a7dc
GangPil Song <92740959+strongfeel@users.noreply.github.com>
2024-10-21 23:30:41 +0900

    add: jwt 발급 과정 추가

--- src/user/dto/create-user.dto.ts
+++ src/user/dto/create-user.dto.ts
@@ -1,1 +1,8 @@
-export class CreateUserDto {}
+import { PickType } from '@nestjs/mapped-types';
+
+import { User } from '../entities/user.entity';
+
+export class CreateUserDto extends PickType(User, [
+  'email',
+  'password',
+] as const) {}
--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,26 +1,69 @@
-import { Injectable } from '@nestjs/common';
+import _ from 'lodash';
+import { Repository } from 'typeorm';
+
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+  UnauthorizedException,
+} from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+import { InjectRepository } from '@nestjs/typeorm';
+
 import { CreateUserDto } from './dto/create-user.dto';
-import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User) private userRepository: Repository<User>,
+    private jwtService: JwtService,
+  ) {}
+
+  async login(email: string, password: string) {
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: ['id', 'password'],
+    });
+
+    if (_.isNil(user)) {
+      throw new NotFoundException(`유저를 찾을 수 없습니다. ID: ${email}`);
+    }
+
+    if (user.password !== password) {
+      throw new UnauthorizedException(
+        `유저의 비밀번호가 올바르지 않습니다. ID: ${email}`,
+      );
+    }
+
+    const payload = { id: user.id };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return accessToken;
   }
 
-  findAll() {
-    return `This action returns all user`;
+  async create(createUserDto: CreateUserDto) {
+    const existUser = await this.findOne(createUserDto.email);
+    if (!_.isNil(existUser)) {
+      throw new ConflictException(
+        `이미 가입된 ID입니다. ID: ${createUserDto.email}`,
+      );
+    }
+
+    const newUser = await this.userRepository.save(createUserDto);
+
+    const payload = { id: newUser.id };
+    const accessToken = await this.jwtService.signAsync(payload);
+    return accessToken;
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  checkUser(userPayload: any) {
+    return `유저 정보: ${JSON.stringify(userPayload)}}`;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
-  }
-
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  private async findOne(email: string) {
+    return await this.userRepository.findOne({
+      where: { email },
+      select: ['email', 'createdAt', 'updatedAt'],
+    });
   }
 }
